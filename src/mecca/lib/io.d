66a379912d2fe2d0c91511b79c4a41040c71df3d
Jacob Carlborg <doob@me.com>
2018-07-19 22:41:59 +0200

    Move `O_DSYNC` and `O_RSYNC` to mecca.platform.os.linux
    These symbols don't exist on Darwin.

--- src/mecca/lib/io.d
+++ src/mecca/lib/io.d
@@ -5,7 +5,7 @@ module mecca.lib.io;
 
 import core.sys.posix.unistd;
 public import core.sys.posix.fcntl :
-    O_ACCMODE, O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_EXCL, O_TRUNC, O_APPEND, O_DSYNC, O_RSYNC, O_SYNC, O_NOCTTY;
+    O_ACCMODE, O_RDONLY, O_WRONLY, O_RDWR, O_CREAT, O_EXCL, O_TRUNC, O_APPEND, O_SYNC, O_NOCTTY;
     /* O_NOATIME, O_NOFOLLOW not included because not defined */
 import core.sys.posix.fcntl;
 import std.algorithm : min, move;
@@ -17,6 +17,7 @@ import mecca.lib.memory;
 import mecca.lib.string;
 import mecca.log;
 import mecca.platform.x86;
+import mecca.platform.os : O_DSYNC, O_RSYNC;
 
 private extern(C) nothrow @trusted @nogc {
     int pipe2(ref int[2], int flags);
--- src/mecca/platform/os/linux.d
+++ src/mecca/platform/os/linux.d
@@ -553,3 +553,4 @@ mixin template hookSyscall(alias F, Syscall nr, alias traceFunc, SyscallTracePoi
 }+/
 
 public import core.sys.linux.sys.mman : MAP_POPULATE;
+public import core.sys.posix.fcntl : O_DSYNC, O_RSYNC;
